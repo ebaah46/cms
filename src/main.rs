474083f6ab2a04fd898e0f6c84139b47b12278dc
ebaah <ebaah46@yahoo.com>
2025-08-09 15:15:11 +0300

    refactoring types

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,17 @@ mod parsing;
 use std::fs;
 use std::error::Error;
 
-use regex::Regex;
-// use crate::parsing::content_parser::parse_content;
+
+use crate::parsing::content_parser::parse_content;
+
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let json_string = fs::read_to_string("src/data.json")?;
-    println!("{}", json_string);
-    // let parsed_node = parse_content(json_string);
-
-    // let text = "listGames(genre=action|adventure)";
-    let text = "favoriteGames";
-    let re = Regex::new(r"^([^\(]+)(?:\(([^)]*)\))?$")?;
-    let caps = re.captures(text);
-    if let Some(caps) = caps {
-        println!("{:?}", &caps[1]);
-    }
+    let file = "src/data.json";
+    let json_string = fs::read_to_string(&file)?;
+    // println!("{}", json_string);
+    let mut parsed = parse_content(json_string)?;
+    parsed.add_name(&file);
+    println!("{:#?}", parsed);
+    
     Ok(())
 }
